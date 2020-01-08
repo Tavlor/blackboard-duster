7fc812eed7ba359c1d143f6cbb499c062df514df
Taylor Smith <tavlor@protonmail.com>
2020-01-07 18:22:33 -0600

    removed non-functioning wait_on_element()

--- blackboard-duster.py
+++ blackboard-duster.py
@@ -13,6 +13,9 @@ TODO:
         if needed
     - ignore useless navpane elements - add custom ignore arg
     - log downloaded items, so they can be ignored next time
+    - allow user to choose browser
+    - allow user to define additional MIME types, or automatically add
+        them as encountered
 ~*~ """
 
 import argparse
@@ -28,6 +31,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 global args
 global driver
+# array to hold encountered MIME types
+global mime_types
+
 navpane_ignore = {'Announcements', 'Calendar', 'My Grades'}
 
 
@@ -54,37 +60,40 @@ def argparse_setup():
     #    '-p', '--print', metavar='level',type=int,
     #    action='store',default=0,
     #    help='Priority level for printing, see --log.')
-    # parser.add_argument(
-    #    '-b', '--browser', metavar='level',
-    #    help='browser to use.')
+    parser.add_argument(
+        '-b', '--browser', metavar='level', default='firefox',
+        help='browser to use - either "firefox" or "chrome". Currently, \
+       only firefox is supported; that will change in the future')
     args = parser.parse_args()
 # end argparse_setup()
 
 
-def wait_for_element(locator, delay_time=3, exit_on_fail=False,
-                     msg_success='', msg_fail=''):
-    """waits until given item appears, optionally exit on failure
-
-    locator is a tuple: (By.constant, string)
-    if messages are empty no newline will be generated
-    returns the element waited for
-    """
+def get_ff_profile():
+    """ sets up a profile to configure download options"""
+    # many thanks to the author of this article
+    # https://yizeng.me/2014/05/23/download-pdf-files-automatically-in-firefox-using-selenium-webdriver/
     global args
-    global driver
-    element = None
-    try:
-        element = WebDriverWait(
-            driver, args.delay * delay_time).until(
-            EC.presence_of_element_located(locator))
-        if msg_success:
-            print(msg_success)
-    except TimeoutException:
-        if msg_fail:
-            print(msg_fail)
-        if exit_on_fail:
-            driver.quit()
-            exit()
-    return element
+    profile = webdriver.FirefoxProfile()
+    # enable custom save location
+    profile.set_preference('browser.download.folderList', 2)
+    # set save location
+    # TODO expand save path
+    profile.set_preference('browser.download.dir', args.save)
+    # disable showing the download manager
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+    # disable save popup
+    profile.set_preference(
+        'browser.helperApps.neverAsk.saveToDisk', 'application/pdf')
+    # disable built-in PDF viewer
+    profile.set_preference('pdfjs.disabled', True)
+    # disable scanning for plugins - in case there's other file viewers
+    profile.set_preference('plugin.scan.plid.all', False)
+    return profile
+
+
+def get_ch_profile():
+    """ TODO implement chrome profile"""
+    pass
 
 
 def manual_login():
@@ -103,21 +112,14 @@ def manual_login():
 
 def accept_cookies():
     """if the cookie notice appears, click 'accept'"""
-    # try:
-    #     element = WebDriverWait(driver, args.delay * 3).until(
-    #         EC.presence_of_element_located((By.ID, 'agree_button'))
-    #     )
-    #     print('I am accepting the cookie notice, I hope that is ok!')
-    #     element.click()
-    # except TimeoutException:
-    #     print('I did not see a cookie notice.')
-    cookie_bttn = wait_for_element(
-        (By.ID, 'agree_button'),
-        msg_success='I am accepting the cookie notice, I hope that is ok!',
-        msg_fail='I did not see a cookie notice.')
-    if cookie_bttn is not None:
-        print("clicking cookie button!")
-        cookie_bttn.click
+    try:
+        element = WebDriverWait(driver, args.delay * 3).until(
+            EC.presence_of_element_located((By.ID, 'agree_button'))
+        )
+        print('I am accepting the cookie notice, I hope that is ok!')
+        element.click()
+    except TimeoutException:
+        print('I did not see a cookie notice.')
 
 
 def get_courses_info():
@@ -128,18 +130,15 @@ def get_courses_info():
     global driver
     global args
     result = []
-    # try:
-    #     course_links=WebDriverWait(driver, args.delay * 5).until(
-    #         EC.presence_of_element_located((By.CSS_SELECTOR,
-    #                                         'div#div_25_1 a'))
-    #     )
-    # except TimeoutException:
-    #     print('I did not see your course list! Aborting')
-    #     driver.quit()
-    #     exit()
-    wait_for_element(
-        (By.CSS_SELECTOR, 'div#div_25_1 a'), exit_on_fail=True,
-        msg_fail='I did not see your course list! Aborting')
+    try:
+        course_links = WebDriverWait(driver, args.delay * 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR,
+                                            'div#div_25_1 a'))
+        )
+    except TimeoutException:
+        print('I did not see your course list! Aborting')
+        driver.quit()
+        exit()
     course_links = driver.find_elements_by_css_selector(
         'div#div_25_1 a')
     for link in course_links:
@@ -156,10 +155,15 @@ def get_navpane_info():
     each dict contains {'name', 'url'}
     """
     global driver
-    wait_for_element(
-        (By.CSS_SELECTOR, 'ul#courseMenuPalette_contents'),
-        exit_on_fail=True,
-        msg_fail='I could not access the navpane! Aborting')
+    try:
+        WebDriverWait(driver, args.delay * 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR,
+                                            'ul#courseMenuPalette_contents'))
+        )
+    except TimeoutException:
+        print('I could not access the navpane! Aborting')
+        driver.quit()
+        exit()
     page_links = driver.find_elements_by_css_selector(
         'ul#courseMenuPalette_contents a')
     result = []
@@ -173,32 +177,38 @@ def get_navpane_info():
 
 
 def parse_page(page_url):
-    """returns an array of dicts for each file link in page
+    """returns an array of dicts for each downloadable link in page
 
     recursivly handles folders
     takes the url as a string
-    each dict contains {'name', 'url'}
+    each dict contains {'name', 'url', 'link_element'}
     """
     global driver
     driver.get(page_url)
-    content_list = wait_for_element(
-        (By.CSS_SELECTOR, 'ul#content_listContainer'),
-        msg_fail='This page does not have a content list.')
-    if content_list is not None:
-        return []
+    result = []
+    try:
+        WebDriverWait(driver, args.delay * 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR,
+                                            'ul#content_listContainer'))
+        )
+    except TimeoutException:
+        print('This page does not have a content list.')
+        return result
     page_content = driver.find_elements_by_css_selector(
         'ul#content_listContainer li')
-    result = []
     for item in page_content:
         i_type = item.find_element_by_css_selector(
             'img').get_attribute('alt')
+        print("    found: " + i_type)
         if i_type == 'File':
             # files are just a link
             result.append({
                 'name': item.find_element_by_css_selector(
                     'a span').text,
                 'url': item.find_element_by_css_selector(
-                    'a').get_attribute('href')
+                    'a').get_attribute('href'),
+                'link_element': item.find_element_by_css_selector(
+                    'a')
             })
         elif i_type == 'Item':
             # items contain attachments
@@ -213,7 +223,7 @@ def parse_page(page_url):
             child_url = item.find_element_by_css_selector(
                 'a').get_attribute('href')
             my_url = page_url
-            result.append(parse_page(child_url))
+            result.append(parse_page(child_url))  # recursion!
             driver.get(my_url)
             pass
     return result
@@ -223,11 +233,19 @@ def main():
     global args
     global driver
     argparse_setup()
-    driver = webdriver.Firefox()
+    if args.browser == 'firefox':
+        driver = webdriver.Firefox(get_ff_profile())
+    elif args.browser == 'chrome':
+        driver = webdriver.Chrome(get_ch_profile())
+    else:
+        print('sorry, but {0:s} is not a supported browser. Aborting'.format(
+            args.browser))
+        exit()
+
     driver.get(args.bb_url)
     # choose a nice size - the navpane is invisible at small widths,
     # but selenium can still see its elements
-    driver.set_window_size(850, 700)
+    driver.set_window_size(1200, 700)
     manual_login()
     print('Alright, I can drive from here.')
     # TODO are links visible behind the cookie notice?
@@ -241,6 +259,7 @@ def main():
         print(course['name'])
         driver.get(course['url'])
         navpane = get_navpane_info()
+        # iterate over each page
         for page in navpane:
             # a few pages have no (downloadable) content, skip them
             if page['name'] in navpane_ignore:
@@ -249,9 +268,12 @@ def main():
             # TODO don't reload the home page
             # TODO skip emails page - different for each school
             print('   ' + page['name'])
+            # iterate over links in page, including subpages (folders)
             for item in parse_page(page['url']):
                 print('    ' + item['name'])
                 print('      ' + item['url'])
+                driver.get(item['url'])
+                # item['link_element'].click()
     print('That is all I could find! You should double check.')
     driver.quit()
 # end main()
